7b26ed53b902125b15a7e7e5abfce53de12367d1
guanluyue <gly20020601@gmail.com>
2023-11-25 18:00:54 -0500

    Add Create modal and Edit model

--- client/src/components/administrativePage.js
+++ client/src/components/administrativePage.js
@@ -1,39 +1,40 @@
 import React, { useState } from 'react';
 import CreatePracticeQuestion from './CreatePracticeQuestion';
+import EditPracticeQuestion from './EditPracticeQuestion';
+import './AdministrativePage.css'; // Importing CSS
 
-function AdministrativePage() {
-  const [showCreateModal, setShowCreateModal] = useState(false);
+const AdministrativePage = () => {
+    const [showCreateModal, setShowCreateModal] = useState(false);
+    const [showEditModal, setShowEditModal] = useState(false);
 
-  const handleSubmitQuestion = (newQuestion) => {
-    fetch('/api/practicequestions', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(newQuestion),
-    })
-    .then(response => response.json())
-    .then(data => {
-      alert('Question created successfully');
-      setShowCreateModal(false);
-    })
-    .catch((error) => {
-      console.error('Error:', error);
-    });
-  };
+    return (
+        <div className="admin-page">
+            <h1 className="page-title">Hi, Admin</h1>
+            <div className="button-container">
+                <button className="admin-button" onClick={() => setShowCreateModal(true)}>Create Practice Questions</button>
+                <button className="admin-button" onClick={() => setShowEditModal(true)}>Edit Practice Questions</button>
+                <button className="admin-button">New Questions From Users</button> {/* Adjust as needed for functionality */}
+            </div>
 
-  return (
-    <div>
-      <h1>Driver Crash Course - Administrative Page</h1>
-      <button onClick={() => setShowCreateModal(true)}>Create Questions</button>
-      <CreatePracticeQuestion 
-        show={showCreateModal} 
-        handleClose={() => setShowCreateModal(false)} 
-        handleSubmit={handleSubmitQuestion} 
-      />
-      {/* TODO: Other components and logic */}
-    </div>
-  );
-}
+            {showCreateModal && (
+                <div className="modal">
+                    <div className="modal-content">
+                        <button className="close-button" onClick={() => setShowCreateModal(false)}>&times;</button>
+                        <CreatePracticeQuestion onClose={() => setShowCreateModal(false)} />
+                    </div>
+                </div>
+            )}
+
+            {showEditModal && (
+                <div className="modal">
+                    <div className="modal-content">
+                        <button className="close-button" onClick={() => setShowEditModal(false)}>&times;</button>
+                        <EditPracticeQuestion onClose={() => setShowEditModal(false)} />
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+};
 
 export default AdministrativePage;
