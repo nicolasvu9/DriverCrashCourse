bc7e56608219a64c9888b519a752ac5887983d94
MatCorner <129108761+MatCorner@users.noreply.github.com>
2023-12-06 17:43:06 -0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -11,12 +11,15 @@ import Footer from "./components/Footer";
 import LoginPage from "./components/LoginPage.js";
 import DashBoard from "./components/Dashboard.js";
 import LandingPage from "./components/LandingPage.js";
+import AdministrativePage from './components/AdministrativePage';
 const App = () => {
   return (
     <Router>
       <TopNav />
       <Routes>
+        <Route path="/" element={<LoginPage />} />
         <Route path="/DashBoard" element={<DashBoard />} />
+        <Route path="/admin" element={<ProtectedRoute element={<AdministrativePage />} />} />
         {/*<Route path="/Practice" element={< />} />*/}
         <Route path="/exam" element={<MockExamPage />} />
         {/*<Route path="/FlashCard" element={< />} />*/}
@@ -33,3 +36,4 @@ const App = () => {
 };
 
 export default App;
+
