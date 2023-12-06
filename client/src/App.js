a4f03b6b5a6ad7030bcb58e8999aabd98aa4fb15
MatCorner <129108761+MatCorner@users.noreply.github.com>
2023-12-06 17:32:12 -0500

    Update App.js
    LogOut redirects to LandingPage

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,34 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LoginPage from './components/LoginPage';
-import AdministrativePage from './components/AdministrativePage'; 
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import TopNav from "./components/TopNav.js";
+import ProgressPage from "./components/progressPage.js";
+import SuggestionPage from "./components/Suggestions";
+import MockExamPage from "./components/MockExamPage";
+//import FlashCardPage from "./FlashCardPage";
 import ProtectedRoute from './utils/ProtectedRoute';
-import Dashboard from './components/Dashboard';
-function App() {
+import Video from "./components/Video";
+import Footer from "./components/Footer";
+import LoginPage from "./components/LoginPage.js";
+import DashBoard from "./components/Dashboard.js";
+import LandingPage from "./components/LandingPage.js";
+const App = () => {
   return (
     <Router>
+      <TopNav />
       <Routes>
-      <Route path="/" element={<LoginPage />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/admin" element={<ProtectedRoute element={<AdministrativePage />} />} />
-        {/* Add other routes here */}
+        <Route path="/DashBoard" element={<DashBoard />} />
+        {/*<Route path="/Practice" element={< />} />*/}
+        <Route path="/exam" element={<MockExamPage />} />
+        {/*<Route path="/FlashCard" element={< />} />*/}
+        <Route path="/Progress" element={<ProgressPage />} />
+        <Route path="/Suggestion" element={<SuggestionPage />} />
+        <Route path="/Video" element={<Video />} />
+        <Route path="/LogOut" element={<LandingPage/>} />
       </Routes>
+
+      <Footer />
     </Router>
   );
-}
+};
 
 export default App;
