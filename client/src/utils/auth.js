867a1f4546695386914ff4584b9d9e5d4370a959
MatCorner <129108761+MatCorner@users.noreply.github.com>
2023-12-06 17:29:57 -0500

    Add files via upload

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -5,6 +5,5 @@ export const logout = () => {
   // Clear the authentication token
   Cookies.remove('access_token');
 
-  // Redirect to the login page or any other desired page
-  window.location.href = '/login';
+  
 };
