8660641227889b830304427d6b5b90b638b186b3
guanluyue <gly20020601@gmail.com>
2023-12-09 15:11:24 -0500

    delete/add flashcard

--- server/routes/flashcards.js
+++ server/routes/flashcards.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
-import{ getFlashcards } from "../controllers/flashcards/flashcards.js";
+import{ addFlashcard, getFlashcards, deleteFlashcard } from "../controllers/flashcards/flashcards.js";
 
-import { verifyToken } from "../middlewares/auth.js";
+import { verifyToken, isAdmin } from "../middlewares/auth.js";
 const router = Router();
 
 router.get("/", [verifyToken], async function (req, res) {
@@ -11,6 +11,25 @@ router.get("/", [verifyToken], async function (req, res) {
     } catch (err) {
         res.status(400).send({ msg: err.message });
     }
+});
+
+router.post("/", [verifyToken, isAdmin], async function (req, res) {
+    try {
+        const newFlashcard = await addFlashcard(req.body);
+        res.send(newFlashcard);
+      } catch (err) {
+        res.status(400).send({ msg: err.message });
+      }
+});
+
+router.delete("/:_id", [verifyToken, isAdmin], async function (req, res) {
+    try {
+        const documentId = req.params._id;
+        const deleteResponse = await deleteFlashcard(documentId);
+        res.send(deleteResponse);
+      } catch (err) {
+        res.status(400).send({ msg: err.message });
+      }
 });
 
 export default router;
